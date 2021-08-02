64034121b6997cbc1f684854e8ea2f49ca645d3a
Akinbobola Olawole <stolaksoftech@yahoo.com>
2021-08-02 10:58:20 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">Online Service</router-link>
+      <router-link to="#" class="navbar-brand">Online Service</router-link>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
-          <router-link to="/" class="nav-link">Home</router-link>
+           <a @click="newlink('/')" class="nav-link">Dashboard</a>
         </li>
         <li class="nav-item">
-          <router-link to="/transactions" class="nav-link">Recenct Transactions</router-link>
+          <a @click="newlink('/transactions')" class="nav-link">Recenct Transactions</a>
         </li>
       </div>
     </nav>
@@ -35,6 +35,9 @@ export default {
   
   methods: {
     
+  newlink(link) {
+        window.location.href = link
+    },  
     
 
   }
