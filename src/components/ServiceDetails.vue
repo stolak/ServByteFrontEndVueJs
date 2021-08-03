028fc92f4a80fb24474cda91f7682e433c540577
Akinbobola Olawole <stolaksoftech@yahoo.com>
2021-08-03 09:46:15 +0100

    Update ServiceDetails.vue

--- src/components/ServiceDetails.vue
+++ src/components/ServiceDetails.vue
@@ -15,9 +15,9 @@
   <p><button @click="payWithPaystack">Order now</button></p>
 </div>
 </template>
+
 <script>
 import DataService from "../services/DataService";
-
 export default {
   name: "add-tutorial",
   data() {
@@ -43,40 +43,29 @@ export default {
           console.log(response.data);
         })
         .catch(e => {
-          console.log(e); 
+          console.log(e);  
         });
     },
     payWithPaystack(e) {
-        
+      
+       console.log(e)
+       // eslint-disable-next-line
     let handler = PaystackPop.setup({
-
     key: 'pk_test_3e3454e5e74080dc46572be6bf9c42096a3f0082', // Replace with your public key
-
     email: this.email?this.email:'stolaksoftech@yahoo.com',//document.getElementById("email-address").value,
-
     amount: this.serviceitem.price*100,//document.getElementById("amount").value * 100,
-
     ref: ''+Math.floor((Math.random() * 1000000000) + 3), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-
      label: this.serviceitem.description,
-
     onClose: function(){
-
       //alert('Window closed.');
       //VerifyTansaction()
-
     },
-
     callback: function(response){
-
       let message = 'Payment complete! Reference: ' + response.reference + 'Message  sent to you email';
-
       alert(message);
        window.location.href = "/"
       //VerifyTansaction()
-
     }
-
   });
   handler.openIframe();
     },
